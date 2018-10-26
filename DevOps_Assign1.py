fccbe2dc50ce1f6f829f136f0665ca6f30080b1f
mat <mat.c.peder@gmail.com>
2018-10-26 13:50:39 +0100

    Fifth Commit

--- DevOps_Assign1.py
+++ DevOps_Assign1.py
@@ -7,6 +7,7 @@ import time
 import boto3
 
 
+# The main menu of the program. Prints all the available selections to choose from
 def main():
     print("Please Choose one of the following")
     print("1. Create an Instance")
@@ -17,21 +18,25 @@ def main():
     print("6. Delete a Bucket")
     print("7. Insert file or image in bucket")
     print("8. SSH into an instance")
-    print("9. SCP check nginx file into instance (run after starting instance)")
+    print("9. SCP check nginx file and index.html into instance (run after starting instance)")
     print("0. Exit")
 
 
+# Refrences code given to us in the labs from Richard
+# Uses user input to ssh into a specified instance and checks for errors
 def sshscript():
     try:
         print("Please enter the IP address of the desired instance")
         ipadd = input()
-        os.system("sudo ssh -i Key.pem ec2-user@" + ipadd)
+        os.system("sudo ssh -t -i Key.pem ec2-user@" + ipadd)
         print("Success!")
 
     except subprocess.CalledProcessError:
         print("SSH failed, please try again")
 
 
+# References code given to us in the labs from Richard
+# runs a command to check if the nginx server is running
 def checknginx():
     try:
         cmd = 'ps -A | grep nginx'
@@ -43,14 +48,18 @@ def checknginx():
         print("Nginx Server IS NOT running")
 
 
+# References code given to us in the labs from Richard
+# Creates instance using the key and adds it to a specified Security Group.
+# Then updates and installs Python3, linux extras, nginx and starts nginx
+# Then it creates a testfile to show it has successfully created and updated the instance
 def createinstance():
     ec2 = boto3.resource('ec2')
     instance = ec2.create_instances(
         ImageId='ami-0c21ae4a3bd190229',
-        KeyName='MatKey2018',
+        KeyName='Key',
         MinCount=1,
         MaxCount=1,
-        SecurityGroupIds=['sg-idnumber'],
+        SecurityGroupIds=['sg-03f820e818a796a0f'],
         UserData='''#!/bin/bash
                     yum update -y
                     yum install python3 -y
@@ -64,17 +73,32 @@ def createinstance():
     instance[0].reload()
     print("Public IP address:", instance[0].public_ip_address)
 
+
+# Script used to scp the check_webserver.py file into the specified instance by asking user for the IP of the instance
+# Also copies the html file into the instance
 def scpcommand():
-    sshcmd = "sudo scp -o StrictHostKeyChecking=no -i Key.pem check_webserver.py ec2-user@"
+    cwcmd = "sudo scp -o StrictHostKeyChecking=no -i Key.pem check_webserver.py ec2-user@"
     direct = ":/home/ec2-user/"
+    indexcmd = "sudo scp -i Key.pem index.html ec2-user@"
     print("Please enter the IP address of the desired instance")
     ipadd = input()
     try:
-        subprocess.run(sshcmd + ipadd + direct, check=True, shell=True)
-        print("Copy successful")
+        subprocess.run(cwcmd + ipadd + direct, check=True, shell=True)
+        time.sleep(10)
+        print("check_webserver.py Copy successful")
+        subprocess.run(indexcmd + ipadd + direct, check=True, shell=True)
+        print("index.html Copy successful")
+        # The index.html must be moved manually after ssh'ing into the instance.
+        # I was unable to have it moved automatically through the command line
+        print("Please remember to overwrite the old index.html file by running: ")
+        print("'sudo mv index.html ../../usr/share/nginx/html'")
     except subprocess.CalledProcessError:
         print("Error running scp, please try again.")
 
+
+# References code given to us in the labs from Richard
+
+# Terminates a specified instance after asking user for the ID of the instance
 def terminateinstance():
     ec2 = boto3.resource('ec2')
     print("Please enter your instance ID: ")
@@ -88,25 +112,31 @@ def terminateinstance():
         main()
 
 
+# References code given to us in the labs from Richard
+# Lists all instances
 def listinstances():
     ec2 = boto3.resource('ec2')
     for instance in ec2.instances.all():
         print(instance.id, instance.state)
 
 
+# References code given to us in the labs from Richard
+# Creates a bucket in S3 and asks for the bucket name
 def createbucket():
     s3 = boto3.resource("s3")
     print("Please enter your bucket name: ")
     bucket_name = input()
     try:
         response = s3.create_bucket(Bucket=bucket_name,
-                                    CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
+                                    CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'}, ACL="public-read")
         print(response)
     except Exception as error:
         print(error + "please try again")
         main()
 
 
+# References code given to us in the labs from Richard
+# Lists all the buckets in the user's S3
 def listbucket():
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
@@ -119,6 +149,9 @@ def listbucket():
         print(error)
 
 
+# References code given to us in the labs from Richard
+# Asks for users bucket name, then asks for the file name to copy and sends it to the bucket
+# Attempted to open the file browser, but haven't completed that yet
 def putbucket():
     s3 = boto3.resource("s3")
     print("Please enter your bucket name: ")
@@ -127,12 +160,14 @@ def putbucket():
     print("Please enter the file/image name: ")
     object_name = input()
     try:
-        response = s3.Object(bucket_name, object_name).put(Body=open(object_name, 'rb'))
+        response = s3.Object(bucket_name, object_name).put(Body=open(object_name, 'rb'), ACL="public-read")
         print(response)
     except Exception as error:
         print(error)
 
 
+# References code given to us in the labs from Richard
+# Deletes the specified bucket
 def deletebucket():
     s3 = boto3.resource('s3')
     print("Please enter your bucket name: ")
@@ -147,6 +182,7 @@ def deletebucket():
 
 loop = True
 
+# While Loop that acts as the menu selection. Each selection calls a different method
 while loop:
     main()
     choice = input("Please enter your choice: ")
@@ -202,3 +238,4 @@ while loop:
 
 if __name__ == '__main__':
     main()
+
